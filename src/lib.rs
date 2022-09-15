8b318904997124d366b77aaf8f8ceb48227f1fed
Tomas Olvecky <tomas.olvecky@gmail.com>
2022-09-15 12:05:33 +0200

    Panic while other request is waiting

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::{
+    sync::atomic::{AtomicUsize, Ordering},
+    time::Duration,
+};
 use worker::*;
 
 mod utils;
@@ -18,11 +21,8 @@ fn log_request(req: &Request) {
 #[event(fetch)]
 pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
     log_request(&req);
-
     utils::set_panic_hook();
-
     let router = Router::new();
-
     router
         .get("/", |_, _| {
             let old = COUNTER.fetch_add(1, Ordering::SeqCst);
@@ -32,6 +32,21 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
             Response::ok(old.to_string())
         })
         .get("/crash", |_, _| panic!("crash"))
+        .get_async("/wait/:delay", |_, ctx| async move {
+            let delay: Delay = match ctx.param("delay").unwrap().parse() {
+                Ok(delay) => Duration::from_millis(delay).into(),
+                Err(_) => return Response::error("invalid delay", 400),
+            };
+
+            let old = COUNTER.fetch_add(1, Ordering::SeqCst);
+
+            assert!(old == 0);
+            // Wait for the delay to pass
+            delay.await;
+
+            let old = COUNTER.load(Ordering::SeqCst);
+            Response::ok(old.to_string())
+        })
         .run(req, env)
         .await
 }
